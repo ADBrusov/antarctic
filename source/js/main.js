1c71a262c85efffdfcc8bf51b001341fea1cfe19
Alexander <ADBrusob96@yandex.ru>
2020-11-09 16:27:38 +0300

    Устраняет баги

--- source/js/main.js
+++ source/js/main.js
@@ -3,15 +3,19 @@
   var pageHeader = document.querySelector('.page-header');
   var headerToggle = document.querySelector('.page-header__toggle');
 
-  pageHeader.classList.remove('page-header--nojs');
+  if (pageHeader) {
+    pageHeader.classList.remove('page-header--nojs');
+  }
 
-  headerToggle.addEventListener('click', function () {
-    if (pageHeader.classList.contains('page-header--closed')) {
-      pageHeader.classList.remove('page-header--closed');
-      pageHeader.classList.add('page-header--opened');
-    } else {
-      pageHeader.classList.add('page-header--closed');
-      pageHeader.classList.remove('page-header--opened');
-    }
-  });
+  if (headerToggle || pageHeader) {
+    headerToggle.addEventListener('click', function () {
+      if (pageHeader.classList.contains('page-header--closed')) {
+        pageHeader.classList.remove('page-header--closed');
+        pageHeader.classList.add('page-header--opened');
+      } else {
+        pageHeader.classList.add('page-header--closed');
+        pageHeader.classList.remove('page-header--opened');
+      }
+    });
+  }
 })();
